74fa35b1ab279d42e85d7a64ab9d2fd8591dfa72
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2023-04-20 23:41:01 -0500

    fix: generator push

--- generate_and_push.sh
+++ generate_and_push.sh
@@ -19,21 +19,17 @@ git config user.name "$USERNAME"
 git config user.email "$USER_EMAIL"
 
 echo git diff-index --quiet HEAD --;
-# Check if changes exist
-# if [[ -z $(git status --porcelain) ]]; then
-#     echo "No changes detected."
-# else
-#     echo "Changes are detected."
-#     # Add, commit, and push the changes
-#     git add .
-#     git commit -m "$COMMIT_NAME"
-#     git pull --rebase origin main
-#     git push origin HEAD
+Check if changes exist
+if [[ -z $(git status --porcelain) ]]; then
+    echo "No changes detected."
+else
+    echo "Changes are detected."
+    # Add, commit, and push the changes
+    git add .
+    git commit -m "$COMMIT_NAME"
+    git pull --rebase origin main
+    git push origin HEAD
 
-#     # Update the version number and publish the updated package to npm
-#     npm version patch # This will automatically commit the changes and create a new version tag
-#     npm publish --access public --otp=$NPM_OTP_TOKEN
-#     git push --follow-tags # Push the changes and the new tag to the remote repository
-# fi
+fi
 
 
